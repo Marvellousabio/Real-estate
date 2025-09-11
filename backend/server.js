f1af1baa9ff44ae365d4227c64e23c573c65f5e2
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-09-11 08:20:15 +0100

    now

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app= express();
 app.use(cors({ origin:[
     "http://localhost:5173",
-     "https://real-estate-iul2.vercel.app/" 
+     "https://real-estate-iul2.vercel.app" 
 
     ]
     }));
